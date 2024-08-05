dda63658dc9f487682b6a40755114eedecbb3c3f
brunoeduardodev <brunoeduardo.dev@gmail.com>
2024-08-05 15:23:35 -0300

    feat: org

--- client/pages/Login.tsx
+++ client/pages/Login.tsx
@@ -3,7 +3,7 @@ import { AuthCard } from "../components/AuthCard";
 import { z } from "zod";
 import { useZodForm } from "../hooks/useZodForm";
 import { AuthCardFooter } from "../components/AuthCardFooter";
-import { TextInput } from "@mantine/core";
+import { TextInput, Text } from "@mantine/core";
 import { Meteor } from "meteor/meteor";
 import { showErrorToast } from "../utils/showErrorToast";
 import { FormContainer } from "../components/FormContainer";
@@ -36,6 +36,9 @@ export const LoginPage = () => {
   return (
     <AuthCard title="Login">
       <FormContainer onSubmit={onSignIn}>
+        <Text>
+          Org
+        </Text>
         <TextInput
           label="Email"
           placeholder="brunomedeiros@gmail.com"
