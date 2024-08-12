47c423cb39344780c7b9e27e978c4cd22742201c
Bruno Medeiros <bruno+2@meteor.com>
2024-08-12 16:16:34 -0300

    updated github

--- client/pages/Login.tsx
+++ client/pages/Login.tsx
@@ -37,7 +37,7 @@ export const LoginPage = () => {
     <AuthCard title="Login">
       <FormContainer onSubmit={onSignIn}>
         <Text>
-          Github
+          Github Updated
         </Text>
         <TextInput
           label="Email"
