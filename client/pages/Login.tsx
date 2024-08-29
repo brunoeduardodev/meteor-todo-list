0c47994e426c62303fac84645cd47fab9c597f4a
Bruno Medeiros <bruno+2@meteor.com>
2024-08-29 12:25:32 -0300

    feat: update

--- client/pages/Login.tsx
+++ client/pages/Login.tsx
@@ -37,7 +37,7 @@ export const LoginPage = () => {
     <AuthCard title="Login">
       <FormContainer onSubmit={onSignIn}>
         <Text>
-          Gitlab 3 Updated! 
+          Update!
         </Text>
         <TextInput
           label="Email"
