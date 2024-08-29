4e62eb40c519c17c5bc1f46edb4c2465a015eed1
Bruno Medeiros <bruno+2@meteor.com>
2024-08-29 13:45:32 -0300

    feat: update with owner

--- client/pages/Login.tsx
+++ client/pages/Login.tsx
@@ -37,7 +37,7 @@ export const LoginPage = () => {
     <AuthCard title="Login">
       <FormContainer onSubmit={onSignIn}>
         <Text>
-          Update!!!
+          Update!!!!!
         </Text>
         <TextInput
           label="Email"
